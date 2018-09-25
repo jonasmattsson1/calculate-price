bf228d8ae0b40035d80ba51a76e16685316c7506
jonas mattsson <jonas.mattsson@widespace.com>
2018-09-25 16:24:33 +0200

    Moved getProductFee and getTodayRebate functions to helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -6,3 +6,25 @@
 export const isToday = (date) => {
     return new Date(date).setHours(0,0,0,0) === (new Date()).setHours(0,0,0,0)
 }
+
+
+/**
+ * Takes an feeConfig object and an productType id, and return the fee
+ * 
+ * @param {Object} feeConfig - Fee config object
+ * @param {number} productTypeId - ProductType id
+ * @return {number} - Fee amount
+ */
+const getProductFee = (feeConfig, productTypeId = 0) => {
+    return feeConfig.find( fee => fee.id === productTypeId)
+}
+
+/**
+ * Get the rebate of the day.
+ * 
+ * @param {Date} date - Date object
+ * @param {number} rebate - 
+ */
+const getTodayRebate = (date, rebate) => {
+    return isToday(date) ? rebate : 0;
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import { productFees, newRebate, userTypes } from './config.js'
-import { isToday } from './helpers.js'
+import { getProductFee, getTodayRebate } from './helpers.js'
 
-
-/**
- * Takes an feeConfig object and an productType id, and return the fee
- * 
- * @param {Object} feeConfig - Fee config object
- * @param {number} productTypeId - ProductType id
- * @return {number} - Fee amount
- */
-const getProductFee = (feeConfig, productTypeId = 0) => {
-    return feeConfig.find( fee => fee.id === productTypeId)
-}
-
-/**
- * Get the rebate of the day.
- * 
- * @param {Date} date - Date object
- * @param {number} rebate - 
- */
-const getTodayRebate = (date, rebate) => {
-    return isToday(date) ? rebate : 0;
-}
 
 /**
  * 
