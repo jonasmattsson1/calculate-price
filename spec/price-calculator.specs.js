58a0da78bfab19f960586148aa609a20c1857aed
jonas mattsson <jonas.mattsson@widespace.com>
2018-09-26 22:49:09 +0200

    Added more tests on price-calculator.specs.js

--- spec/price-calculator.specs.js
+++ spec/price-calculator.specs.js
@@ -1,9 +1,9 @@
 
 import {calculatePrice} from '../src/index.js' 
 
-describe('price calculator', function(){
-	
-	it('should calc price right', function(){
+describe('price-calculator', function(){
+
+	it('should calculate right for: normal user, new product and today', function(){
 		const userType = 0;
 		const productType = 0;
 		const price = 1;
@@ -15,24 +15,63 @@ describe('price calculator', function(){
 	});
 
 
-	it('should return 1 if productType dose not exist', function(){
+	it('should calculate right for: normal user, old product and today', function(){
 		const userType = 0;
-		const productType = 2;
+		const productType = 1;
 		const price = 1;
 		const publishedDate = new Date();
 
-		const expected = 1;
+		const expected = 36;
 		const actual = calculatePrice(userType, productType, price, publishedDate);
 
 		expect(expected).to.equal(actual);
 	});
 
-	it('should return 0 if no parameters are passed', function(){
+	it('should calculate right for: normal user, new product and old date', function(){
+		const userType = 0;
+		const productType = 0;
+		const price = 1;
+		const publishedDate = new Date('2019-01-01');
 
-		const expected = 0;
-		const actual = calculatePrice();
+		const expected = 26;
+		const actual = calculatePrice(userType, productType, price, publishedDate);
 
 		expect(expected).to.equal(actual);
 	});
 
+	it('should calculate right for: company, new product and today', function(){
+		const userType = 1;
+		const productType = 0;
+		const price = 1;
+		const publishedDate = new Date();
+
+		const expected = 11;
+		const actual = calculatePrice(userType, productType, price, publishedDate);
+		expect(expected).to.equal(actual);
+	});
+
+
+	it('should calculate right for: company, old product and today', function(){
+		const userType = 1;
+		const productType = 1;
+		const price = 1;
+		const publishedDate = new Date();
+
+		const expected = 31;
+		const actual = calculatePrice(userType, productType, price, publishedDate);
+
+		expect(expected).to.equal(actual);
+	});
+
+	it('should calculate right for: company, new product and old date', function(){
+		const userType = 1;
+		const productType = 0;
+		const price = 1;
+		const publishedDate = new Date('2019-01-01');
+
+		const expected = 21;
+		const actual = calculatePrice(userType, productType, price, publishedDate);
+
+		expect(expected).to.equal(actual);
+	})
 });
