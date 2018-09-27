91a51744cd990b111c87457f5c29d20b576ae24e
jonas mattsson <jonas.mattsson@widespace.com>
2018-09-27 11:10:49 +0200

    Added test to for invalid parameters

--- spec/price-calculator.specs.js
+++ spec/price-calculator.specs.js
@@ -74,4 +74,16 @@ describe('price-calculator', function(){
 
 		expect(expected).to.equal(actual);
 	})
+
+	it('should return undefined and not throw error if invalid parameters is passed', function(){
+		const userType = 100;
+		const productType = '101';
+		const price = -1;
+		const publishedDate = undefined;
+
+		const expected = undefined;
+		const actual = calculatePrice(userType, productType, price, publishedDate);
+
+		expect(expected).to.equal(actual);
+	})
 });
